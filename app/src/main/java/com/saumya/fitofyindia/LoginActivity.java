0f581e99eca2c86079ad94053b8418d5facf382f
Dheeraj0052 <47526970+Dheeraj0052@users.noreply.github.com>
2019-10-13 20:56:26 +0530

    login done

--- app/src/main/java/com/saumya/fitofyindia/LoginActivity.java
+++ app/src/main/java/com/saumya/fitofyindia/LoginActivity.java
@@ -1,20 +1,30 @@
 package com.saumya.fitofyindia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin;
     EditText etPassword, etEmail;
     TextView tvRegister;
-
+    String email,password;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +33,35 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         etEmail = findViewById(R.id.etEmail);
         tvRegister = findViewById(R.id.tvRegister);
-
+        auth=FirebaseAuth.getInstance();
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                email=etEmail.getText().toString();
+                password=etPassword.getText().toString();
+                if(checkvalidation()){
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful())
+                                    {
+                                        Log.d("tag", "signInWithEmail:success");
+                                        Intent i = new Intent(getBaseContext(), OrganisationMainActivity.class);
+                                        startActivity(i);
 
+                                        finish();
 
-                Intent i = new Intent(getBaseContext(), OrganisationMainActivity.class);
-                startActivity(i);
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(LoginActivity.this,"Aithentication Failed",Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                }
+
             }
         });
 
@@ -48,4 +79,21 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+    private boolean checkvalidation() {
+        boolean check=true;
+
+        if (TextUtils.isEmpty(email))
+        {
+            etEmail.setError("Required");
+            check=false;
+        }
+        if (TextUtils.isEmpty(password))
+        {
+            etPassword.setError("Required");
+            check=false;
+        }
+        return  check;
+    }
+
 }
--- app/src/main/java/com/saumya/fitofyindia/RegisterActivity.java
+++ app/src/main/java/com/saumya/fitofyindia/RegisterActivity.java
@@ -1,17 +1,75 @@
 package com.saumya.fitofyindia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    FirebaseAuth auth;
+    String email, password;
+    EditText etEmail, etPassword;
+    Button btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        auth = FirebaseAuth.getInstance();
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = etEmail.getText().toString();
+                password = etPassword.getText().toString();
+                if (chekvalidation()) {
+                    auth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d("tag", "createUserWithEmail:success");
+
+                                        Intent intent = new Intent(RegisterActivity.this, OrganisationMainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Log.e("tag", "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
 
 
-
+    }
+    private boolean chekvalidation() {
+        boolean check = true;
+        if (TextUtils.isEmpty(email)) {
+            etEmail.setError("Required");
+            check = false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            etPassword.setError("Required");
+            check = false;
+        }
+        return check;
     }
 }
