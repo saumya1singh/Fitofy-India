2892029b94484c92d1bc0792f7ffb2625ee4d0ed
Saumya <singhsaumyas150@gmail.com>
2019-10-15 17:31:41 +0530

    Commit

--- app/src/main/java/com/saumya/fitofyindia/RegisterActivity.java
+++ app/src/main/java/com/saumya/fitofyindia/RegisterActivity.java
@@ -21,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
 public class RegisterActivity extends AppCompatActivity {
     FirebaseAuth auth;
     String email, password , Organisation;
--- app/src/main/java/com/saumya/fitofyindia/SelectActivity.java
+++ app/src/main/java/com/saumya/fitofyindia/SelectActivity.java
@@ -11,6 +11,7 @@ public class SelectActivity extends AppCompatActivity {
 
     Button btnOrganisation, btnIndividual;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
