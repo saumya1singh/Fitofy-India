bea9180d0100db5f7b1ebdbc6aacfad4521feea9
Saumya <singhsaumyas150@gmail.com>
2019-11-24 19:23:20 +0530

    Updated Code

--- app/src/main/java/com/saumya/fitofyindia/IndividualMain.java
+++ app/src/main/java/com/saumya/fitofyindia/IndividualMain.java
@@ -30,7 +30,7 @@ public class IndividualMain extends AppCompatActivity {
         llgym.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent  intent= new Intent(getBaseContext(),Healthactivity.class);
+                Intent  intent= new Intent(getBaseContext(),GymMainActivity.class);
                 startActivity(intent);
             }
         });
